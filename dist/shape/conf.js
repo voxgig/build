83f60349b622424be7eeb80fe7a4b62606d279f7
Richard Rodger <richard@ricebridge.com>
2024-02-22 13:37:03 +0000

    v2.1.0

--- dist/shape/conf.js
+++ dist/shape/conf.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CloudConfShape = void 0;
 const gubu_1 = require("gubu");
-// const { Open, Skip } = Gubu
-const CloudConfShape = (0, gubu_1.Gubu)({
+const { Open } = gubu_1.Gubu;
+const CloudConfShape = (0, gubu_1.Gubu)(Open({
     aws: {
         region: 'us-east-1',
         accountid: 'AWS-ACCOUNT-ID',
     }
-}, { prefix: 'CloudConf' });
+}), { prefix: 'CloudConf' });
 exports.CloudConfShape = CloudConfShape;
 //# sourceMappingURL=conf.js.map
