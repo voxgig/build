32f40c1a78600bd5233d44f98f3da9e490a98abe
Richard Rodger <richard@ricebridge.com>
2024-07-10 18:56:23 +0100

    v3.0.0

---
+++ dist/structure.d.ts
@@ -0,0 +1,5 @@
+declare function entity_valid(model: any): void;
+declare const Structure: {
+    entity_valid: typeof entity_valid;
+};
+export { Structure };
---
+++ dist/structure.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Structure = void 0;
+function entity_valid(model) {
+}
+const Structure = {
+    entity_valid
+};
+exports.Structure = Structure;
+//# sourceMappingURL=structure.js.map
